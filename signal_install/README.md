be0840f02bbe82d27ed50424ac2be8df5d71d694
Sascha Schirra <sashs@scoding.de>
2017-12-05 15:14:21 +0100

    initial commit

---
+++ signal_install/README.md
@@ -0,0 +1,3 @@
+This is a little helper script to download the [signal-desktop](https://signal.org) dep package
+and create PKGBUILD file to install it with pacman.
+
---
+++ signal_install/signal.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+CWD=$(pwd)
+DIR="signal_$(date +'%s')"
+TEMP_PATH="/tmp/$DIR"
+mkdir -p $TEMP_PATH
+cd $TEMP_PATH
+MAIN_URL="https://updates.signal.org/desktop/apt/"
+
+# Download Packages
+PACKAGES_URL="dists/xenial/main/binary-amd64/Packages.bz2"
+wget "$MAIN_URL$PACKAGES_URL"
+bzip2 -d Packages.bz2
+
+# Download deb
+FILE_URL=$(cat Packages | grep Filename | head -n 1 | awk '{print $2}')
+FILE_NAME=$(echo $FILE_URL | awk 'BEGIN { FS = "/" } ; { print $NF }')
+wget "$MAIN_URL$FILE_URL"
+
+
+# Generate PKGBUILD
+HASH=$(sha512sum "$TEMP_PATH/$FILE_NAME" | awk '{print $1}')
+SED="s/HASH_VALUE/$HASH/"
+sed -e "$SED" $CWD/PKGBUILD_TEMPLATE > PKGBUILD
+SED="s/URL_VALUE/file:\/\/\/tmp\/$DIR\/$FILE_NAME/"
+sed -i -e "$SED" PKGBUILD
+
+cp $CWD/signal-desktop.install $TEMP_PATH
+makepkg && sudo pacman -U *.pkg.tar.xz
